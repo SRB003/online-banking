d3cec06581d0ccaae5e9868166479c8e3c200cf3
SRB003 <sahilbhuvad01926@gmail.com>
2024-06-23 10:48:15 +0530

    initial file and folder structure

---
+++ app/layout.tsx
@@ -0,0 +1,30 @@
+import type { Metadata } from "next";
+import { Inter , IBM_Plex_Serif} from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] , variable: '--font-inter' });
+const ibmplesxserif = IBM_Plex_Serif({ 
+  subsets: ["latin"] , 
+  weight: ['400', '700'],
+  variable: '--font-ibm-plex-serif' 
+});
+
+export const metadata: Metadata = {
+  title: "Online Banking",
+  description: "Modern Banking Platform for Everyone.",
+  icons: {
+    icon: "/icons/logo.svg"
+  }
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${inter.variable} ${ibmplesxserif.variable}`}>{children}</body>
+    </html>
+  );
+}
